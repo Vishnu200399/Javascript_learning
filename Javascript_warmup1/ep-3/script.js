d31f10ee472561702d97aa3937580ec1675ee645
vishnu vardhan <hackingup3@gmail.com>
2023-09-17 10:38:07 +0530

    fixed the hover movement

--- Javascript_warmup1/ep-3/script.js
+++ Javascript_warmup1/ep-3/script.js
@@ -16,10 +16,24 @@ document.querySelector('#center').addEventListener("mousemove", throttleFunction
  div.classList.add('imagediv');
  div.style.left = dets.clientX+'px';
  div.style.top = dets.clientY+'px';
- var div = document.createElement("img");
+ var img = document.createElement("img");
  img.setAttribute("src","https://images.unsplash.com/photo-1694619363789-62a774d3a5d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80");
  div.appendChild(img);
  document.body.appendChild(div);
+gsap.to(img,{
+  y:"0",
+  ease:Power1,
+  duration: .2
+}) 
+
+
+gsap.to(img,{
+  y:"100",
+  delay: .6,
+  ease:Power2,
+
+}) 
+
  setTimeout(function(){
   div.remove();
  },2000)
